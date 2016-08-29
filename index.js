49f663ece59d7490132e8b962a88607a78f08283
Amanda Baker <mandykatebaker@Amandas-MacBook-Pro.local>
2016-08-29 16:31:35 +1200

    comment out unused redux stuff

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+// import { createStore } from 'redux'
+// import { Provider } from 'react-redux'  // we'll def be using these in the future
 
 render(<App name='mixBank' />, document.querySelector('main'))
 console.log('welcome to mixBank')
