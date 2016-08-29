79c9321f5517f0c2c9eb89bbc23beeb10ef4c137
Amanda Baker <mandykatebaker@Amandas-MacBook-Pro.local>
2016-08-29 16:07:56 +1200

    moved into table

--- components/account.js
+++ components/account.js
@@ -8,21 +8,50 @@ class Account extends Component {
     this.state = initialState
   }
 
-  // displayInformation (account) {
-  //   return account.map(function(item){
-  //       return
-  //       <p>{item}</p>
-  //   })
-  // }
-
   render () {
     console.log("initial state!", initialState)
     return (
-        <div>
-            <h2>This is the transaction information for account {this.state.account.transactions[0].id}</h2>
-            <p>${this.state.account.transactions[0].amount}</p>
-            <p>{this.state.account.transactions[0].description}</p>
-        </div>
+
+<div className="container">
+        <h2>Transaction Information </h2>
+        <p>This is the transaction information for account {this.state.account.transactions[0].id} and {this.state.account.transactions[1].id} </p>
+  <table className="table table-bordered">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Amount</th>
+        <th>Description</th>
+        <th>From-Account</th>
+        <th>To-Account</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr className="success">
+        <td>{this.state.account.transactions[0].id}</td>
+        <td>${this.state.account.transactions[0].amount}</td>
+        <td>{this.state.account.transactions[0].description}</td>
+        <td>{this.state.account.transactions[0].from_account_id}</td>
+        <td>{this.state.account.transactions[0].to_account_id}</td>
+
+      </tr>
+      <tr className="danger">
+        <td>{this.state.account.transactions[1].id}</td>
+        <td>${this.state.account.transactions[1].amount}</td>
+        <td>{this.state.account.transactions[1].description}</td>
+        <td>{this.state.account.transactions[1].from_account_id}</td>
+        <td>{this.state.account.transactions[1].to_account_id}</td>
+      </tr>
+
+    </tbody>
+  </table>
+</div>
+
+
+        // <div>
+        //     <h2>This is the transaction information for account {this.state.account.transactions[0].id}</h2>
+        //     <p>${this.state.account.transactions[0].amount}</p>
+        //     <p>{this.state.account.transactions[0].description}</p>
+        // </div>
     )
   }
 }
