162e2b01da74a7dc8d13a8c5f3ee8227a9ff2d74
Amanda Baker <mandykatebaker@Amandas-MacBook-Pro.local>
2016-08-30 11:32:26 +1200

    added get transactions for account

--- database/queries.js
+++ database/queries.js
@@ -1,9 +1,8 @@
-const database = require('../state')
 
 //actuall should use knex db for database, not client side dummy data
 
 const getTransactionsForAccount = (id) => {
-  return database
+  return
   // in the future, this will need a join query from actual db
 }
 
