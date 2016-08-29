ce68b4a3871ee1ec6541086d2a0df2c683ee6b26
Amanda Baker <mandykatebaker@Amandas-MacBook-Pro.local>
2016-08-29 12:42:34 +1200

    added dummy data to index.js

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-render(<App name='mixBank' />, document.querySelector('main'))
-console.log('welcome to mixBank')
+const initialState = {
+    account:
+      {
+        id: 1,
+        transactions: [
+            {id: 503,
+            amount: 456,
+            desctiption: 'purhcase bk',
+            from_account_id:1,
+            to_account_id:2
+            },
+            {id: 504,
+            amount: 56700,
+            desctiption: 'weekly pay from burger king',
+            from_account_id:2,
+            to_account_id:1
+            },
+        ]
+    }
+}
+
+const reducer = function (state = initialState , action) {
+  return state
+}
+
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <App name='mixBank' />
+  </Provider>,
+  document.querySelector('main')
+)
+
